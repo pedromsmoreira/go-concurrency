9521cac56ffa85ea0913a11513d33eb2f7d4b6f6
Pedro Moreira <pedromsmoreira51@gmail.com>
2021-10-18 20:53:08 +0100

    feat: add finder1 and 2 using go routine

--- main.go
+++ main.go
@@ -1,12 +1,16 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
 func main() {
 	theMine := []string{"rock", "ore", "ore", "rock", "ore"}
-	foundOre := finder(theMine)
-	minedOre := miner(foundOre)
-	smelter(minedOre)
+	go finder1(theMine)
+	go finder2(theMine)
+
+	<-time.After(time.Second * 5)
 }
 
 func finder(mine []string) []string {
@@ -14,9 +18,34 @@ func finder(mine []string) []string {
 	for o := 0; o < len(mine); o++ {
 		if mine[o] == "ore" {
 			fo = append(fo, mine[o])
+			fmt.Println("From Finder found ore!")
 		}
 	}
 	fmt.Printf("From Finder: %v\n", fo)
+	return fo
+}
+
+func finder1(mine []string) []string {
+	fo := make([]string, 0)
+	for o := 0; o < len(mine); o++ {
+		if mine[o] == "ore" {
+			fo = append(fo, mine[o])
+			fmt.Println("From Finder1 found ore!")
+		}
+	}
+	fmt.Printf("From Finder1: %v\n", fo)
+	return fo
+}
+
+func finder2(mine []string) []string {
+	fo := make([]string, 0)
+	for o := 0; o < len(mine); o++ {
+		if mine[o] == "ore" {
+			fo = append(fo, mine[o])
+			fmt.Println("From Finder2 found ore!")
+		}
+	}
+	fmt.Printf("From Finder2: %v\n", fo)
 	return fo
 }
 
